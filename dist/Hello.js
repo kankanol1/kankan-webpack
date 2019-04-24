445b90cd18e2969fa1d8fd1f990f34205c0cad90
lidianzhong <lidianzhong@gl-data.com>
2019-04-24 11:56:14 +0800

    Initial commit

---
+++ dist/Hello.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const React = require("react");
+exports.Hello = (props) => React.createElement("h1", null,
+    "Hello from ",
+    props.compiler,
+    " and ",
+    props.framework,
+    "!");
+//# sourceMappingURL=Hello.js.map
